4076ea97baa4982b9de131280023a06c22400e7c
AnwarHossainSR <mahedianwar@gmail.com>
2022-03-23 19:35:25 +0600

    setup firebase and redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
 import "./assets/styles/index.css";
+import { store } from "./redux/store";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+        <ToastContainer />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
