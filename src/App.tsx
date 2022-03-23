5ef13b1b96c6aa66e0c6a1c2f537b190688fefb0
AnwarHossainSR <mahedianwar@gmail.com>
2022-03-23 17:54:36 +0600

    install dependencies and setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { Route, Switch } from "react-router-dom";
+import pageRender from "./pageRender";
+import Home from "./pages";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/:page" component={pageRender} exact />
+        <Route path="/:page/:id" component={pageRender} exact />
+      </Switch>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/pageRender.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const pageRender = () => {
+  return <div>pageRender</div>;
+};
+
+export default pageRender;
